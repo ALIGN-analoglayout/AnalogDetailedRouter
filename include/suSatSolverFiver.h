b7404f1959ef103e3894e5dfd6576d12fb41dd62
Steven Burns <smburns47@gmail.com>
2019-07-16 14:28:15 -0700

    Really removing it

--- include/suSatSolverFiver.h
+++
@@ -1,6 +0,0 @@
-// see LICENSE
-//! \author Ryzhenko Nikolai, 10984646, nikolai.v.ryzhenko@intel.com
-//! \date   Thu Sep 28 09:45:43 2017
-
-//! \file   suSatSolverFiver.h
-//! \brief  A header of the class suSatSolverFiver.
--- src/suSatSolverFiver.cpp
+++
@@ -1,7 +0,0 @@
-// see LICENSE
-//! \author Ryzhenko Nikolai, 10984646, nikolai.v.ryzhenko@intel.com
-//! \date   Thu Sep 28 09:45:53 2017
-
-//! \file   suSatSolverFiver.cpp
-//! \brief  A collection of methods of the class suSatSolverFiver.
-

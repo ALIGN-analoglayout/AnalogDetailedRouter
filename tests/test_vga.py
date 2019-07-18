0725f1daaea63ad5519d87e31d7e3e8842377326
Steven Burns <smburns47@gmail.com>
2019-07-18 06:04:49 -0700

    Fix CI and a test

--- tests/test_vga.py
+++ tests/test_vga.py
@@ -68,3 +68,31 @@ def test_two():
             proc_diff.communicate()
 
         assert proc_diff.returncode == 0
+
+def test_three():
+    with working_directory('tests/vga3'):
+        cmd = ['../../bin/amsr.exe','-file','INPUT/ctrl.txt']
+
+        with Popen( cmd, stdout=PIPE) as proc:
+            p = re.compile( r'.*Net (\S+) has no opens\.\s*$')
+            p2 = re.compile( r'.*open.*')
+            p3 = re.compile( r'.*ISSUE.*')
+
+            for line in proc.stdout:
+                line = line.decode().rstrip('\n')
+                if p.match(line):
+                    pass
+#                    print(line)
+                elif p2.match(line):
+                    print("Other", line)
+                elif p3.match(line):
+                    print("ISSUE", line)
+
+        assert proc.returncode == 0
+
+        diff = ['diff','-r','out','out-gold']
+
+        with Popen( cmd, stdout=PIPE) as proc_diff:
+            proc_diff.communicate()
+
+        assert proc_diff.returncode == 0
